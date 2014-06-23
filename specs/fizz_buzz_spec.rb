f46bdea1133853e4ecc122c684db0c02c2b46690
Dante <dante.alighierimds@gmail.com>
2014-06-22 22:47:00 -0300

    Broke test to see error in travis

--- specs/fizz_buzz_spec.rb
+++ specs/fizz_buzz_spec.rb
@@ -3,7 +3,7 @@ require "./fizz_buzz"
 
 class FizzBuzzTest < Test::Unit::TestCase
   def test_methods_is_fizz
-    assert is_fizz(3)
+    assert is_fizz(10)
   end
   def test_methods_is_buzz
     assert is_buzz(5)
